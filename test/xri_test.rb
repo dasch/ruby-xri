a1cfb84972084f3a7c278a83c9241cc94572d3b0
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2008-09-21 21:24:47 +0200

    Added test helper method.

--- test/xri_test.rb
+++ test/xri_test.rb
@@ -5,68 +5,74 @@ require 'lib/xri'
 class XRITest < Test::Unit::TestCase
 
   def test_string_representation
-    assert_equal "=john.doe", XRI.new("=john.doe").to_s
+    assert_equal "=john.doe", xri("=john.doe").to_s
   end
 
   def test_valid_xri_should_not_fail
     assert_nothing_raised do
-      XRI.new("=foobar")
+      xri("=foobar")
     end
   end
 
   def test_uri_form
-    assert_equal "=john.doe", XRI.new("xri://=john.doe").to_s
+    assert_equal "=john.doe", xri("xri://=john.doe").to_s
   end
 
   def test_empty_xri_should_fail
     assert_raise XRI::InvalidIdentifierException do
-      XRI.new("")
+      xri("")
     end
   end
 
   def test_persistent_global_context_symbol
-    assert !XRI.new("=john.doe").persistent?
-    assert  XRI.new("=!1000.a1b2.93d2.8c73").persistent?
+    assert !xri("=john.doe").persistent?
+    assert  xri("=!1000.a1b2.93d2.8c73").persistent?
   end
 
   def test_xri_with_only_global_context_symbol_should_fail
     %W[! @ = +].each do |xri|
       assert_raise XRI::InvalidIdentifierException do
-        XRI.new(xri)
+        xri(xri)
       end
     end
   end
 
   def test_xri_with_spaces_should_fail
     assert_raise XRI::InvalidIdentifierException do
-      XRI.new("=foo bar")
+      xri("=foo bar")
     end
   end
 
   def test_person_iname
-    xri = XRI.new("=john.doe")
+    xri = xri("=john.doe")
     assert  xri.person?
     assert !xri.organization?
     assert !xri.general_public?
   end
 
   def test_organization_iname
-    xri = XRI.new("@acme.corp")
+    xri = xri("@acme.corp")
     assert !xri.person?
     assert  xri.organization?
     assert !xri.general_public?
   end
 
   def test_general_public_iname
-    xri = XRI.new("+address")
+    xri = xri("+address")
     assert !xri.person?
     assert !xri.organization?
     assert  xri.general_public?
   end
 
   def test_equality
-    assert_equal XRI.new("=john.doe"), XRI.new("=john.doe")
-    assert_not_equal XRI.new("=john.doe"), XRI.new("=jane.doe")
+    assert_equal xri("=john.doe"), xri("=john.doe")
+    assert_not_equal xri("=john.doe"), xri("=jane.doe")
+  end
+
+protected
+
+  def xri(str)
+    XRI.new(str)
   end
 
 end
